b995a0ceace2839740b07ff00b97799af7b6e86c
seonmin <seonmin1414@gmail.com>
2024-09-27 11:17:47 +0900

    전체적인 css, 및 html 생성, 수정, 공통 영역 설정

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function(){
+    const sw_visual = new Swiper(".sw-visual", {
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+    })
+})
